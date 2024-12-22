c3d808d0be6820bb3bc6130ec279a1312c9872ec
Asad Aziz <9146069+asadaziz95@users.noreply.github.com>
2024-12-22 17:54:45 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Welcome to our react <app>import.meta.env.TEST_VAR</app>
+      </div>
+    </>
+  )
+}
+
+export default App
